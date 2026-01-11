bd98c7a95bce7727b0ac70275afacc9311401d5f
victor-thrivhe <victor@thrivhe.com>
2026-01-11 15:53:15 +0100

    redditCard feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import NavigationHeader from './components/navigationHeader';
+import RedditCard from './components/RedditCard';
 
 function App() {
- 
+
   return (
-    <div className="App"> 
-     <NavigationHeader /> 
+    <div className="App">
+      <NavigationHeader />
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+        <RedditCard
+          votes="35.8k"
+          title="Getting paid on the Wall of Death in Rajkot, India"
+          image="https://placehold.co/600x400?text=Wall+of+Death"
+          author="mohiemen"
+          timePosted="7 hours ago"
+          comments="689"
+        />
+      </div>
     </div>
 
   );
