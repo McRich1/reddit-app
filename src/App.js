46b561820a4b575ea7f410f56c8fc1f7c32f4cb6
Richie <mcrich.md@gmail.com>
2026-01-11 20:27:06 +0000

    nav and app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import NavigationHeader from './components/navigationHeader';
+import PostFeed from './components/Postfeed';
 
 function App() {
  
   return (
     <div className="App"> 
-     <NavigationHeader /> 
+     <NavigationHeader />
+     <PostFeed />
+     <div>
+        <h1>Img</h1>
+        <img src="https://external-preview.redd.it/pRMGh7nFkVkwaJCpJeDj_y_XqQYawHnZA7T73yhm1xI.jpeg?width=140&amp;height=70&amp;auto=webp&amp;s=79dd10e27718d8ae44b07170011f812b7c5aba4a" alt="testImage" />
+      </div> 
     </div>
 
   );
--- src/components/navigationHeader.jsx
+++ src/components/navigationHeader.jsx
@@ -21,4 +21,4 @@
     }
 
     export default NavigationHeader;
-   
+   //added a comment
